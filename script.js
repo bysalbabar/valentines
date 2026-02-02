4dece7df054f19416bad4dc36305164a0a8f6dbe
Bysal Babar <bysalbabar786@gmail.com>
2026-02-02 19:40:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function scrollToNext() {
-  document.querySelector('.message').scrollIntoView({ behavior: 'smooth' });
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", moveButton);
+noBtn.addEventListener("click", moveButton);
+
+function moveButton() {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
 }
 
-function yes() {
-  document.getElementById("reply").innerHTML =
-    "You just made me the happiest person ❤️🥰";
+function yesClicked() {
+  document.getElementById("message").innerHTML =
+    "Yayyy 😍 I knew it! Happy Valentine’s Day ❤️🌹";
 }
